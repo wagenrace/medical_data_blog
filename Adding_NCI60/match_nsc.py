8f5b05d9d79dc0911b337c696506be642d44a37d
tomnijhof <tom.nijhof@cytosmart.com>
2022-03-14 21:06:16 +0100

    replace chem_names with pubchem names
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- Adding_NCI60/match_nsc.py
+++ Adding_NCI60/match_nsc.py
@@ -10,6 +10,9 @@ chem_names = pd.read_csv(
     header=None,
     names=["NSC", "Name", "Name Type"],
 )
+pubchem_names = pd.read_csv(
+    path_join("nsc_from_pubchem", "results", "nsc2synonym.csv")
+)[["synonym", "NSC"]]
 GI50 = pd.read_csv(path_join("data", "GI50.csv"))
 
 all_nsc = GI50["NSC"].unique()
@@ -19,13 +22,24 @@ port = "7687"
 pswd = input("password")
 
 graph = Graph("bolt://localhost:" + port, auth=(user, pswd))
-results = pd.DataFrame(columns=["NSC", "MeshLabel", "MeshType", "MeshNumResults", "NumberTerms"])
+results = pd.DataFrame(
+    columns=["NSC", "MeshLabel", "MeshType", "MeshNumResults", "NumberTerms"]
+)
 
-for nsc_number in tqdm(all_nsc[:500]):
-    all_chem_names = chem_names.loc[chem_names["NSC"] == nsc_number]["Name"]
-    all_chem_names = [i.lower() for i in all_chem_names]
+for nsc_number in tqdm(all_nsc):
+    # # Chem names
+    # all_chem_names = chem_names.loc[chem_names["NSC"] == nsc_number]["Name"]
+    # all_chem_names = [i.lower() for i in all_chem_names]
+
+    # NSC as names
     nsc_names = [f"nsc{nsc_number}", f"nsc {nsc_number}", f"nsc-{nsc_number}"]
-    all_synonyms = all_chem_names + nsc_names
+
+    # # Pubchem
+    all_pubchem_names = pubchem_names.loc[pubchem_names["NSC"] == nsc_number]["synonym"]
+    all_pubchem_names = [i.lower() for i in all_pubchem_names]
+
+    # Combine names
+    all_synonyms = nsc_names + all_pubchem_names  # + all_chem_names
     cypher_all_synonyms = f"""[{','.join([f'"{i}"' for i in all_synonyms])}]"""
 
     # Find all terms
@@ -70,7 +84,7 @@ for nsc_number in tqdm(all_nsc[:500]):
             ignore_index=True,
         )
         continue
-    
+
     # Find preferred SCR Chemical
     response = graph.run(
         f"""MATCH (t:ns0__Term)-[*1..2]-(c:ns0__Concept)<-[:ns0__preferredConcept]-(preferred_scr:ns0__SCR_Chemical) WHERE toLower(t.ns0__prefLabel) IN {cypher_all_synonyms} RETURN preferred_scr"""
@@ -98,13 +112,13 @@ for nsc_number in tqdm(all_nsc[:500]):
 
     # No results found
     results = results.append(
-            {
-                "NSC": nsc_number,
-                "MeshLabel": None,
-                "MeshType": None,
-                "MeshNumResults": 0,
-                "NumberTerms": num_terms,
-            },
-            ignore_index=True,
-        )
+        {
+            "NSC": nsc_number,
+            "MeshLabel": None,
+            "MeshType": None,
+            "MeshNumResults": 0,
+            "NumberTerms": num_terms,
+        },
+        ignore_index=True,
+    )
 results.to_csv(path_join("results", "nsc_gi502mesh.csv"))
